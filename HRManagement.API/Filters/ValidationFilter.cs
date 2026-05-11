bc8c1b636275c0b081d61e74c50c37737af0c624
Khushi Tomar <22BCS13743@cuchd.in>
2026-05-11 11:00:43 +0530

    changed

--- HRManagement.API/Filters/ValidationFilter.cs
+++ HRManagement.API/Filters/ValidationFilter.cs
@@ -1,9 +1,4 @@
-﻿// HRManagement.API/Filters/ValidationFilter.cs
 
-// This filter runs BEFORE the controller action
-// It checks if the data sent by user is valid
-// If not valid → returns 400 immediately
-// Controller code never runs
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -12,20 +7,17 @@ namespace HRManagement.API.Filters
 {
     public class ValidationFilter : IActionFilter
     {
-        // Runs BEFORE the action
         public void OnActionExecuting(
             ActionExecutingContext context)
         {
             if (!context.ModelState.IsValid)
             {
-                // Cancel the action and return 400
                 context.Result =
                     new BadRequestObjectResult(
                         context.ModelState);
             }
         }
 
-        // Runs AFTER the action — nothing needed here
         public void OnActionExecuted(
             ActionExecutedContext context)
         { }
--- HRManagement.API/Program.cs
+++ HRManagement.API/Program.cs
@@ -44,8 +44,37 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
+<<<<<<< Updated upstream
     app.UseSwagger();
     app.UseSwaggerUI();
+=======
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+           
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+>>>>>>> Stashed changes
 }
 
 app.UseHttpsRedirection();
